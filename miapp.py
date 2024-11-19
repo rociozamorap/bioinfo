58d4657d7ee9983a8974fab302d722b4f68b5f42
rociozamorap <rociozamoraperalta@gmail.com>
2024-11-19 00:00:18 -0700

    Update miapp.py

--- miapp.py
+++ miapp.py
@@ -1,21 +1,27 @@
-# Importar bibliotecas necesarias
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
+import py3Dmol
+import requests
+from stmol import showmol
+import json
 
-# Título de la aplicación
+def get_protein_info(prot):
+    req = requests.get(f'https://data.rcsb.org/rest/v1/core/entry/{prot}/')
+    prot_data = json.loads(req.text)
+    title = prot_data["struct"]["title"]
+    descriptor = prot_data["struct"]["pdbx_descriptor"]
+    return descriptor, title
+
+st.sidebar.title('Show Proteins')
+
 st.title("Selector de Gráficos")
 
-# Crear opciones para seleccionar
 options = ["Seno", "Coseno", "Tangente", "Logaritmo"]
 selection = st.radio("Selecciona un gráfico:", options)
 
-# Función para generar gráficos
 def generate_graph(option):
     fig, ax = plt.subplots()  # Crear una figura
     x = np.linspace(0, 10, 100)  # Valores para el eje x
 
-    # Generar el gráfico según la opción seleccionada
     if option == "Seno":
         ax.plot(x, np.sin(x), label="Seno", color="blue")
     elif option == "Coseno":
@@ -30,7 +36,6 @@ def generate_graph(option):
     ax.grid(True)
     return fig
 
-# Mostrar el gráfico seleccionado
 try:
     fig = generate_graph(selection)
     st.pyplot(fig)  # Mostrar el gráfico en Streamlit

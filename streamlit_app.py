ec25f499e1801e1eea6fa77a54679fed09e28490
rociozamorap <rociozamoraperalta@gmail.com>
2024-11-18 22:47:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,19 +12,9 @@ def get_protein_info(prot):
     return descriptor, title
 
 st.sidebar.title('Show Proteins')
-bcolor = st.sidebar.color_picker('Pick A Color', '#DBDEDB')
 protein = st.sidebar.text_input('Enter protein:', "")
-style = st.sidebar.selectbox('style',['stick','line','cross','sphere','cartoon','clicksphere'])
-spin = st.sidebar.checkbox('Spin', value = False)
 if protein != "":
     st.sidebar.markdown(f'**{get_protein_info(protein)[0]}**')
     st.sidebar.markdown(f'**{get_protein_info(protein)[1]}**')
     xyzview = py3Dmol.view(query='pdb:'+protein)
-    xyzview.setStyle({style:{'color':'spectrum'}})
-    xyzview.setBackgroundColor(bcolor)
-    if spin:
-        xyzview.spin(True)
-    else:
-        xyzview.spin(False)
-    xyzview.zoomTo()
-    showmol(xyzview,height=500,width=800)
+else: showmol(xyzview,height=500,width=800)
